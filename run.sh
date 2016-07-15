cc4b862d156e870533ec5f491406cad0f39935ba
mnod <mnod@example.com>
2016-07-15 10:51:43 +0900

    add pub key

--- run.sh
+++ run.sh
@@ -4,4 +4,13 @@ PASS=`pwgen -s 12 1`
 echo ${PASS}
 echo docker:${PASS} | chpasswd
 
+if [ $# -eq 1 ]; then
+  sshdir=~docker/.ssh
+  keyfile=$sshdir/authorized_keys
+  mkdir $sshdir ; chmod 755 $sshdir
+  touch $keyfile ; chmod 600 $keyfile
+  echo $1 >> $sshdir/authorized_keys
+  chown -R docker:docker $sshdir
+fi
+
 /usr/sbin/sshd -D
